9d3b2e66d6d53e3b6c9baedb8187354aa7724282
azsgit <10238943+azsgit@users.noreply.github.com>
2025-08-17 00:25:05 +0900

    wip

--- manage/bin/params.sh
+++ manage/bin/params.sh
@@ -1,5 +1,5 @@
+pthdoc="${pthtop}"/export/cnf/xoxxox/docenv_envlcp
 cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envlcp.txt
 cnfcmp="${pthtop}"/export/cnf/xoxxox/docker_tttlcp.yml
-pthdoc="${pthtop}"/export/cnf/xoxxox/docker
-imgtgt='xoxxox_envlcp:001'
+imgtgt='xoxxox/envlcp:001'
 cnttgt='xoxxox_tttlcp'
